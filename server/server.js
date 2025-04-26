d62f49c9105f7ca61756843e0dd3c695f1106818
edwin yue <53625790+edwaddle@users.noreply.github.com>
2025-04-26 13:56:30 -0700

    frontend port is 5173, backend port is 3001

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
 // Create Express app
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
